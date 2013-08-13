ab55bf48f05174005c311404353f91b252aa078e
Phoenix Nemo <mrphoenixlzx@gmail.com>
2013-08-13 08:21:53 +0800

    Translation: Vietnamese(vi)

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var express = require('express')
   , i18n = require('i18n');
 
 i18n.configure({
-    locales:['en-US', 'zh-CN', 'zh-TW', 'ja-JP'],
+    locales:['en-US', 'zh-CN', 'zh-TW', 'ja-JP', 'vi'],
     defaultLocale: config.language,
     directory: './i18n',
     updateFiles: false,
